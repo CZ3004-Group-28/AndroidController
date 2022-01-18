9327ea5a52e2dcaaa658db7b602add4cacba5b25
Qi Xiang <qixyqix@outlook.com>
2022-01-18 15:35:34 +0800

    Add toggle bluetooth functionality

--- app/src/main/java/com/example/androidcontroller/BluetoothFragment.java
+++ app/src/main/java/com/example/androidcontroller/BluetoothFragment.java
@@ -1,5 +1,7 @@
 package com.example.androidcontroller;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,58 +9,77 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BluetoothFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class BluetoothFragment extends Fragment {
+    private BluetoothAdapter bluetoothAdapter;
+    private boolean bluetoothOn;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Button btnToggleBluetooth;
+    private Button btnSearchBluetooth;
 
     public BluetoothFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BluetoothFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BluetoothFragment newInstance(String param1, String param2) {
-        BluetoothFragment fragment = new BluetoothFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        bluetoothOn = false;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View rootView =  inflater.inflate(R.layout.fragment_bluetooth, container, false);
+
+        btnToggleBluetooth = rootView.findViewById(R.id.bluetooth_btn_toggle);
+        btnToggleBluetooth.setOnClickListener(v -> {
+            toggleBluetooth();
+        });
+        btnSearchBluetooth = rootView.findViewById(R.id.bluetooth_btn_search);
+        initializeBluetooth();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bluetooth, container, false);
+        return rootView;
+    }
+
+    private void initializeBluetooth(){
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(bluetoothAdapter == null){
+            Toast.makeText(getActivity(), "Bluetooth not supported",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        btnToggleBluetooth.setEnabled(true);
+        if(bluetoothAdapter.isEnabled()) {
+            bluetoothOn = true;
+        }
+        updateBluetoothControlButtons();
+    }
+
+    private void toggleBluetooth(){
+        //Toggle the status
+        bluetoothOn = !bluetoothOn;
+        if(bluetoothOn){
+            bluetoothAdapter.enable();
+        }else{
+            bluetoothAdapter.disable();
+        }
+        updateBluetoothControlButtons();
+    }
+
+    private void searchBluetooth(){
+
+    }
+
+    private void updateBluetoothControlButtons(){
+        if(bluetoothOn){
+            btnToggleBluetooth.setText("Bluetooth: ON");
+            btnSearchBluetooth.setEnabled(true);
+        }else{
+            btnToggleBluetooth.setText("Bluetooth: OFF");
+        }
     }
 }
