ec7a036f2a286dc234de11bc10c0888df758f249
zoemin2 <yzoemin243@gmail.com>
2022-02-12 02:46:53 +0800

    prevent fragment state loss

--- app/src/main/java/com/example/androidcontroller/MainActivity.java
+++ app/src/main/java/com/example/androidcontroller/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.androidcontroller;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
 import com.example.androidcontroller.databinding.ActivityMainBinding;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,33 +21,57 @@ public class MainActivity extends AppCompatActivity {
     //FOR BOTTOM NAVIGATION BAR
     //https://www.youtube.com/watch?v=Bb8SgfI4Cm4
     ActivityMainBinding binding;
+    private String[] TAB_TITLE;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
-        //Set listener for item selected
-        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-            switch (item.getItemId()){
-                case R.id.navigation_home:
-                    replaceFragment(homeFragment);
-                    break;
-                case R.id.navigation_bluetoth:
-                    replaceFragment(bluetoothFragment);
-                    break;
+        setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+
+        ViewPager2 viewPager2 = findViewById(R.id.view_pager);
+        //help to preload and keep the other fragment
+        viewPager2.setOffscreenPageLimit(3);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this);
+        viewPager2.setAdapter(adapter);
+        viewPager2.setUserInputEnabled(false);
+
+        TAB_TITLE = adapter.getTabTitles();
+        tabLayout.setSelectedTabIndicator(R.color.black);
+
+        new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText(TAB_TITLE[position]);
             }
-            return true;
-        });
+        }).attach();
+
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//
+//        //Set listener for item selected
+//        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+//            switch (item.getItemId()){
+//                case R.id.navigation_home:
+//                    replaceFragment(homeFragment);
+//                    break;
+//                case R.id.navigation_bluetoth:
+//                    replaceFragment(bluetoothFragment);
+//                    break;
+//            }
+//            return true;
+//        });
 
         //Default to home fragment
-        replaceFragment(homeFragment);
+        //replaceFragment(homeFragment);
     }
 
-    private void replaceFragment(Fragment fragment){
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_layout,fragment);
-        fragmentTransaction.commit();
-    }
+//    private void replaceFragment(Fragment fragment){
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        fragmentTransaction.replace(R.id.frame_layout,fragment);
+//        fragmentTransaction.commit();
+//    }
 }
